c1bae9ab7c1a5b96f5f22c3941c49b0852bfaa3c
brfer98 <bruno98fernandez@gmail.com>
2025-12-27 00:04:52 +0100

    header and footer components created

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+import { Header } from './shared/header/header';
+import { Footer } from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, Header, Footer],
   templateUrl: './app.html',
   // template: '<h1>ola</h1>',
   styleUrl: './app.scss'
