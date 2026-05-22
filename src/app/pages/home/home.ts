0ec41ad99fc89b17b0d04004624a0eb2df5a094a
brfer98 <bruno98fernandez@gmail.com>
2026-05-23 00:10:45 +0200

    feat - smooth scroll added

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import textos from '../../../assets/data/home.json';
+import gsap from 'gsap';
+import { App } from '../../app';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +9,23 @@ import textos from '../../../assets/data/home.json';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
+
+export class Home implements AfterViewInit{
 	// datos recibidos del json
   text = textos;
   jobs = this.text.es.experience.company.reverse();
   
   svgWidth: number = 40;
+
+  // gsap animations
+  ngAfterViewInit(){
+    // gsap.to(".hero-btn", {rotation: 360, x: 0, duration: 1});
+  }
+
+  scrollFooter() {
+    if (App.lenis) {
+      App.lenis.scrollTo("#footer");
+    }
+  }
+  
 }
