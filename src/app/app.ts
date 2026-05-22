36906c52d39521778eea0089e622717c8d0b42fb
brfer98 <bruno98fernandez@gmail.com>
2026-05-23 00:00:03 +0200

    build - lenis and gsap installation and configuration

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { AfterViewInit, Component, signal } from '@angular/core';
 import { Home } from './pages/home/home';
 import { Footer } from './components/footer/footer';
+import Lenis from "lenis"
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +12,26 @@ import { Footer } from './components/footer/footer';
   // template: '<h1>ola</h1>',
   styleUrl: './app.scss'
 })
-export class App {
+
+export class App implements AfterViewInit{
   protected readonly title = signal('portfolio-bruno');
+  public static lenis:Lenis;
+
+  ngAfterViewInit(){
+    gsap.registerPlugin(ScrollTrigger);
+    App.lenis = new Lenis({
+      lerp: 0.1,
+      smoothWheel: true,
+      wheelMultiplier: 1
+    });
+    
+    App.lenis.on("scroll", ScrollTrigger.update);
+    gsap.ticker.add((time) => {
+      App.lenis.raf(time * 1000)
+    });
+    gsap.ticker.lagSmoothing(0);
+
+  }
+
+
 }
