a3c499a0437723a7fce8fd2e68a7e7c717886dc2
brfer98 <bruno98fernandez@gmail.com>
2026-05-22 13:41:21 +0200

    fix app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Home } from './pages/home/home';
-import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [Home, Header, Footer],
+  imports: [Home, Footer],
   templateUrl: './app.html',
   // template: '<h1>ola</h1>',
   styleUrl: './app.scss'
