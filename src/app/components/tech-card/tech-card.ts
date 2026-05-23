a3a303020de452caa2b51752e3fc667aede14ce0
brfer98 <bruno98fernandez@gmail.com>
2026-05-23 17:18:55 +0200

    style - technologies section redesigned

---
+++ src/app/components/tech-card/tech-card.ts
@@ -0,0 +1,23 @@
+import { Component, inject, input, computed, Inject } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-tech-card',
+  imports: [],
+  templateUrl: './tech-card.html',
+  styleUrl: './tech-card.scss',
+})
+export class TechCard {
+  private sanitizer = inject(DomSanitizer);
+
+  svgWidth: number = 40;
+
+  tituloSvg = input.required<string>();
+  svgRaw = input.required<string>();
+
+  svgSeguro = computed<SafeHtml>(() => {
+    return this.sanitizer.bypassSecurityTrustHtml(this.svgRaw());
+  });
+  
+
+}
