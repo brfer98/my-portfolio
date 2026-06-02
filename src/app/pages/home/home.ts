b3fb521ff06fb9d3f02bb0894afd6f96268e242b
brfer98 <bruno98fernandez@gmail.com>
2026-06-03 00:28:18 +0200

    feat: add animations

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { afterNextRender, Component } from '@angular/core';
 import textos from '../../../assets/data/home.json';
 import gsap from 'gsap';
 import { App } from '../../app';
@@ -12,7 +12,7 @@ import { TechCard } from '../../components/tech-card/tech-card';
   styleUrl: './home.scss',
 })
 
-export class Home implements AfterViewInit{
+export class Home{
 	// datos recibidos del json
   text = textos;
   jobs = this.text.es.experience.company.reverse();
@@ -20,28 +20,39 @@ export class Home implements AfterViewInit{
 
   columnas = new Array(3);
   
+  constructor() {
+    afterNextRender(() => {
+      // espera a que la pagina se haya renderizado y luego ejecuta esto
+      gsap.from(".hero-fade-in", {
+        opacity: 0, 
+        y: 20,
+        duration: 3, 
+        ease: "power3.out",
+        stagger: 0.4
+      })
+      gsap.from(".hero-btn", {
+        opacity: 0, 
+        y: 20,
+        duration:5, 
+        ease: "power2.out"
+      })
 
-
-  // gsap animations
-  ngAfterViewInit(){
-    // gsap.to(".hero-btn", {rotation: 360, x: 0, duration: 1});
-
-    gsap.from(".gsap-hero-fade-in", {
-      opacity: 0, 
-      y: 20,
-      duration: 3, 
-      ease: "power3.out",
-      stagger: 0.4
-    })
-
-    gsap.from(".hero-btn", {
-      opacity: 0, 
-      y: 20,
-      duration:5, 
-      ease: "power2.out"
-    })
+      // array de todos los elementos con la clsae gsap-fade-in
+      gsap.utils.toArray(".gsap-fade-in").forEach((e: any) => {
+        gsap.from(e, {
+          scrollTrigger: {
+            trigger: e,
+            start: "top 80%"
+          },
+          opacity: 0,
+          y: 20,
+          duration: 3, 
+          ease: "power3.out"
+        });
+      })
+    });
   }
-
+  
   scrollFooter() {
     if (App.lenis) {
       App.lenis.scrollTo("#footer");
