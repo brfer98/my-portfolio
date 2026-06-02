bef5a00b9155344fd79569d31b9d26854307ec77
brfer98 <bruno98fernandez@gmail.com>
2026-06-02 19:12:18 +0200

    style: add hero animations

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -25,6 +25,21 @@ export class Home implements AfterViewInit{
   // gsap animations
   ngAfterViewInit(){
     // gsap.to(".hero-btn", {rotation: 360, x: 0, duration: 1});
+
+    gsap.from(".gsap-hero-fade-in", {
+      opacity: 0, 
+      y: 20,
+      duration: 3, 
+      ease: "power3.out",
+      stagger: 0.4
+    })
+
+    gsap.from(".hero-btn", {
+      opacity: 0, 
+      y: 20,
+      duration:5, 
+      ease: "power2.out"
+    })
   }
 
   scrollFooter() {
