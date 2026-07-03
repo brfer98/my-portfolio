6ec1d7c75e29b919069cb82666d5a03aee20a261
brfer98 <bruno98fernandez@gmail.com>
2026-07-04 00:31:50 +0200

    refactor: delete bootstrap classes

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import textos from '../../../assets/data/footer.json';
+import { NbDisplay, NbTitle } from '@ng-brutalism/ui';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [NbDisplay, NbTitle],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
