4b36c5e22e8912bcbe2847805a60fe4249b5faef
brfer98 <bruno98fernandez@gmail.com>
2026-07-07 22:34:53 +0200

    core: add scroll functions to header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import texts from '../../../assets/data/header.json';
+import { App } from '../../app';
+import { NbSection, NbSurface, NbSplit, NbButton, NbCluster } from "@ng-brutalism/ui";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [NbSection, NbSurface, NbSplit, NbButton, NbCluster],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
   text = texts;
+
+
+    scrollToExperience() {
+      if (App.lenis) {
+        App.lenis.scrollTo("#experience");
+      }
+    }
+    scrollToStack() {
+      if (App.lenis) {
+        App.lenis.scrollTo("#stack");
+      }
+    }
+    scrollToProjects() {
+      if (App.lenis) {
+        App.lenis.scrollTo("#projects");
+      }
+    }
 }
