f60c86b48997e3d19aff1b95b37e9f972652b4b9
brfer98 <bruno98fernandez@gmail.com>
2026-07-08 16:43:19 +0200

    feat: add info to project cards

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,28 +1,33 @@
-import { afterNextRender, Component } from '@angular/core';
+import { afterNextRender, Component, OnInit } from '@angular/core';
 import textos from '../../../assets/data/home.json';
 import gsap from 'gsap';
 import { Header } from '../../components/header/header';
 import { App } from '../../app';
 import { TechCard } from '../../components/tech-card/tech-card';
-import { ɵNbBorderCapability, NbText, NbSplit, NbStack, NbAvatar, NbDisplay, NbCluster, NbChip, NbTitle, NbCard, NbCardTitle, NbCardHeader, NbCardContent, NbIconButton, NbCardActions, NbCardFooter, NbButton, NbMediaFrame, NbChipGroup } from '@ng-brutalism/ui';
+import { HomeInterfaceTs } from './models/home.interface.ts';
+import { ɵNbBorderCapability, NbText, NbSplit, NbStack, NbAvatar, NbDisplay, NbCluster, NbChip, NbTitle, NbCard, NbCardTitle, NbCardHeader, NbCardContent, NbIconButton, NbCardActions, NbCardFooter, NbButton, NbMediaFrame, NbChipGroup, NbIcon, NbStatusDot, NbBadge, NbStat, NbSurface } from '@ng-brutalism/ui';
 
 
 @Component({
   selector: 'app-home',
-  imports: [Header, TechCard, ɵNbBorderCapability, NbText, NbSplit, NbAvatar, NbStack, NbDisplay, NbCluster, NbChip, NbTitle, NbCard, NbCardTitle, NbCardHeader, NbCardContent, NbIconButton, NbCardActions, NbCardFooter, NbButton, NbMediaFrame, NbChipGroup ],
+  imports: [Header, TechCard, ɵNbBorderCapability, NbText, NbSplit, NbAvatar, NbStack, NbDisplay, NbCluster, NbChip, NbTitle, NbCard, NbCardTitle, NbCardHeader, NbCardContent, NbIconButton, NbCardActions, NbCardFooter, NbButton, NbMediaFrame, NbChipGroup, NbIcon, NbStatusDot, NbBadge, NbStat, NbSurface],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 
-export class Home{
+export class Home implements OnInit{
 	// datos recibidos del json
   text = textos;
+
   jobs = this.text.es.experience.company.reverse();
   techs = this.text.es.technologies.techs;
   projects = this.text.es.projects;
   projects_cards = this.text.es.projects.projects;
+  contact = this.text.es.contact;
 
   columnas = new Array(3);
+
+  proyectos: HomeInterfaceTs[] = [];
   
   constructor() {
     afterNextRender(() => {
@@ -55,6 +60,10 @@ export class Home{
         });
       })
     });
+  }
+
+  ngOnInit(): void {
+    this.proyectos = this.text.es.projects.projects;
   }
   
   scrollFooter() {
