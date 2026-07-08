9aca2cae783d4d0d488f23768c37530f75cbd894
brfer98 <bruno98fernandez@gmail.com>
2026-07-08 16:39:47 +0200

    refactor: add home interface for json data

---
+++ src/app/pages/home/models/home.interface.ts.ts
@@ -0,0 +1,10 @@
+export interface HomeInterfaceTs {
+project_title: string;
+    project_description: string;
+    project_img: string;        
+    technologies: string[];     
+    project_link: string | null;
+    project_category: string;
+    available: boolean;         
+    state: string;
+}
