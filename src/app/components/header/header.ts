e5411b0f25d0a461dcc876da685dc32b028703f1
brfer98 <bruno98fernandez@gmail.com>
2026-07-13 02:33:26 +0200

    feat: add new header style

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import texts from '../../../assets/data/header.json';
 import { App } from '../../app';
 import { NbSection, NbSurface, NbSplit, NbButton, NbCluster, NbIcon, NbText } from "@ng-brutalism/ui";
@@ -11,21 +11,25 @@ import { NbSection, NbSurface, NbSplit, NbButton, NbCluster, NbIcon, NbText } fr
 })
 export class Header {
   text = texts;
+  isMenuOpen = signal(false);
 
-
-    scrollToExperience() {
-      if (App.lenis) {
-        App.lenis.scrollTo("#experience");
-      }
+  toggleMenu(): void {
+    // 3. Usa .update() o .set() para cambiar su valor
+    this.isMenuOpen.update(open => !open);
+  }
+  scrollToExperience() {
+    if (App.lenis) {
+      App.lenis.scrollTo("#experience");
     }
-    scrollToStack() {
-      if (App.lenis) {
-        App.lenis.scrollTo("#stack");
-      }
+  }
+  scrollToStack() {
+    if (App.lenis) {
+      App.lenis.scrollTo("#stack");
     }
-    scrollToProjects() {
-      if (App.lenis) {
-        App.lenis.scrollTo("#projects");
-      }
+  }
+  scrollToProjects() {
+    if (App.lenis) {
+      App.lenis.scrollTo("#projects");
     }
+  }
 }
